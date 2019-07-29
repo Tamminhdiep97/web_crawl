ae456114c4bfa587daca6e7ddeaa0874bc6d32dd
unknown <you@example.comtamdiepminh97@gmail.com>
2019-07-29 23:46:52 +0700

    trying on recursion and failed, testing on queue and make link out of scrapy class

--- crawl/Code/crawl1.py
+++ crawl/Code/crawl1.py
@@ -1,34 +1,49 @@
 import scrapy
-"""
+
+
+
+child_txt =['computer_science#compact'] #to check existance
+child_queue =['computer_science#compact','artificial_intelligence#compact']  #first in first out, insert at tail
+
 def link_process(a):
     replace_string=a.replace(' ','_')
     return replace_string
-file_object = open("structure.txt","r",encoding="utf-8")
-a= file_object.read()
-print(a)
-"""
-child_txt =[]
-class BrickSetSpider(scrapy.Spider):
-    name = "brickset_spider"
-    start_urls=['https://cso.kmi.open.ac.uk/topics/computer_science#compact']
-    
-    def parse(self, response):
-        b=[]
-        set_selector=response.xpath("//table[1]")
-        i=0
-        for title in set_selector.css('.compact-topic'):
-            i=i+1
-            child = title.css('a ::text').get()
-            b.append(child)
-            #yield {'title': title.css('a ::text').get()}
-        global child_txt
-        child_txt = b
 
+def next_link(queue):
+    queue.pop(0)
+    if(len(queue)==0):
+        return
+    return queue[0]
+
+class BrickSetSpider(scrapy.Spider):
+    global child_txt
+    global child_queue
+    name = "brickset_spider"
+    file_object = open("structure.txt","r",encoding="utf-8")
+    b=file_object.readline().strip('\n')
+    if(len(child_queue)==0): 
+        exit()
+    start_urls=[b+child_queue[0]]
+        #print(start_urls)
+    def parse(self, response):
+        bd=[]
+        set_selector=response.xpath("//table[1]")
+        j=0
+        for title in set_selector.css('.compact-topic'):
+            j=j+1
+            child = link_process(title.css('a ::text').get()+'#compact')
+            bd.append(child)
+            #yield {'title': title.css('a ::text').get()}
+        child_txt = bd
+            #print(child_txt)
         title_page =response.css(".display-5::text").get()
         no_decendent = response.css(".card-body>p::text").getall()
         content = response.css(".card-text::text").get()
         link_wiki=response.css("#wikipedia::attr(href)").get()
-        print(link_wiki)
+        print(title_page)
+        child_queue.pop(0)
+    BrickSetSpider(scrapy.Spider)
+        #print(link_wiki)
 """
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
